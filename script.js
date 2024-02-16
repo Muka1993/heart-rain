eceb6ac0f298b701445e1722bd41d5d7807f7910
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2024-02-16 18:11:45 -0300

    Criando novo repositório

---
+++ script.js
@@ -0,0 +1,20 @@
+// JAVASCRIPT
+
+function rain(){
+    let heart = document.querySelector('.heart');
+    let e = document.createElement('div');
+    e.classList.add('drop');
+    heart.appendChild(e);
+
+
+        let left = Math.floor(Math.random() * 300);
+
+        e.style.left = left + 'px';
+
+    setTimeout(function(){
+        heart.removeChild(e);
+    },5000);
+}
+setInterval(function(){
+    rain()
+},50);
